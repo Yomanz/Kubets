aa2deff2dcc42b27ab37fca354cd1b45708a7a4a
Daave <daave@widgetbot.io>
2019-12-30 17:16:56 +0000

    New file to represent testing

--- test/index.ts
+++ test/index.ts
@@ -1,36 +1,90 @@
-import {CommandRequest, CommandResponse, GeneralReceiver, GeneralSender, ReceiverType} from "../src/rpc";
-import { TextDecoder } from 'text-encoding';
-import {Util} from "../src/classes";
-import {Request, Response} from "../src/protos";
+import {GeneralReceiver, GeneralSender, ReceiverType} from "../src/rpc";
+import {TextDecoder} from 'text-encoding';
+import {Config, Util} from "../src/classes";
+import {Event, Request, Response, Subscribe} from "../src/protos";
+import {Giver, Publisher, Subscriber} from "../src/pubsub";
 
-process.env.KubeMQServerAddress = '127.0.0.1';
 
-const reciever = new GeneralReceiver('hello-world-sender', 'testing_Command_channel', ReceiverType.Commands, undefined, 5000);
-const sender = new GeneralSender('hello-world-sender', 'testing_Command_channel', ReceiverType.Commands, 5000);
+const reciever = new GeneralReceiver({
+	host: '127.0.0.1',
+	port: 50000,
+	channel: 'testing_Command_channel',
+	client: 'hello-world-receiver',
+	type: ReceiverType.Query,
+	defaultTimeout: 50000
+});
+const pubsub = new Publisher({
+	host: Config.get('KubeMQServerAddress', '127.0.0.1'),
+	port: Config.get('KubeMQServerPort', 50000),
+	channel: 'testing_Command_channel',
+	client: 'hello-world-sender',
+	type: ReceiverType.Query,
+	defaultTimeout: 50000
+});
+const eater = new Subscriber({
+	host: Config.get('KubeMQServerAddress', '127.0.0.1'),
+	port: Config.get('KubeMQServerPort', 50000),
+	channel: 'testing_Command_channel',
+	client: 'hello-world-sender',
+	type: ReceiverType.Query,
+	defaultTimeout: 50000
+});
+const sender = new GeneralSender({
+	host: Config.get('KubeMQServerAddress', '127.0.0.1'),
+	port: Config.get('KubeMQServerPort', 50000),
+	channel: 'testing_Command_channel',
+	client: 'hello-world-sender',
+	type: ReceiverType.Query,
+	defaultTimeout: 50000
+});
+
+function generate_random_data1(size: number){
+	let chars = 'abcdefghijklmnopqrstuvwxyz'.split('');
+	let len = chars.length;
+	let random_data = [];
+
+	while (size--) {
+		random_data.push(chars[Math.random()*len | 0]);
+	}
+
+	return random_data.join('');
+}
+
+const bigData = Buffer.from(generate_random_data1(2216839));
 
 let request = new Request();
-request.setBody(Util.StringToByte('boris'));
+request.setBody(Buffer.from("lmao pass it over"));
+request.setTimeout(10000);
+
+const event = new Event();
+event.setBody(Buffer.from("yeah are u"));
+event.setClientid('gateway');
+event.setChannel('test');
 
 // console.log('moving up and down, side to side like a rollercoaster.');
 reciever.subscribe((cmd: Request) => {
 	console.log('Receiver got a hit for a message!');
 	let body = cmd.getBody();
 
-	if (typeof body !== 'string') body = new TextDecoder().decode(body);
-	console.log(body);
 
-	reciever.ack(cmd).then((snd: any) => {
+	// if (typeof body !== 'string') body = new TextDecoder().decode(body);
+	// console.log(body);
+	const res = new Response();
+	res.setBody(bigData)
+
+	reciever.ack(cmd, res).then((snd: any) => {
 		console.log('Receiver has acknowledged request.!')
 	}).catch(console.log)
 }, (e: any) => {
 	console.log('sub error')
-	console.log(e)
+	// console.log(e)
 });
 
 setTimeout(() => {
-	console.log('We are about to send the message: ' + request.getBody());
+	console.log('We are about to send a message');
 	sender.send(request).then((res: Response) => {
-		console.log('Sender has sent message successfully sent and it was acknowledged by the receiver.')
+		console.log('Giver has sent message successfully sent and it was acknowledged by the receiver.')
+		console.log(new TextDecoder().decode(<Uint8Array>res.getBody()).substr(0, 20))
 		if (res.getError()) {
 			console.log('Response error:' + res.getError());
 			return;
@@ -41,4 +95,4 @@ setTimeout(() => {
 		console.log('pub error')
 		console.log(e)
 	});
-}, 1000);
+}, 2000);
