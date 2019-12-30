102e166a55eaaa8a3adef6eb2377cfd59d66c510
Daave <daave@widgetbot.io>
2019-12-30 17:13:46 +0000

    PubSub classes

--- src/pubsub/general/publisher.ts
+++ src/pubsub/general/publisher.ts
@@ -1,3 +1,11 @@
-export class Publisher {
+import {PubSub} from "../pubsub";
+import {Settings} from "../../interfaces";
+import {Event, Result} from "../../protos/generated";
 
+export class Publisher extends PubSub {
+	constructor(settings: Settings) { super(settings) }
+
+	send(event: Event): Promise<Result> {
+		return super.send(event);
+	}
 }
--- src/pubsub/general/subscriber.ts
+++ src/pubsub/general/subscriber.ts
@@ -1,3 +1,14 @@
-export class Subscriber {
+import {Settings} from "../../interfaces";
+import {PubSub} from "../pubsub";
 
+export class Subscriber extends PubSub {
+	constructor(settings: Settings) { super(settings) }
+
+	subscribe(reqHandler: (...args: any[]) => void, errorHandler: (...args: any[]) => void) {
+		super.subscribe(reqHandler, errorHandler);
+	}
+
+	unsubscribe() {
+		super.unsubscribe();
+	}
 }
